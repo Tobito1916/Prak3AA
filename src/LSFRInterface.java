1664d7a146b94cc41d731f3234876ec70600e6cf
Tobito1916 <jonas.hoesl@gmx.de>
2018-06-13 11:57:17 +0200

    Set Up

---
+++ src/LSFRInterface.java
@@ -0,0 +1,3 @@
+public interface LSFRInterface {
+   String run (int times);
+}
---
+++ src/LSFRUtil.java
@@ -0,0 +1,15 @@
+public class LSFRUtil implements LSFRInterface{
+    int [] pos;
+    public LSFRUtil(int[] pos){
+        this.pos = pos;
+
+    }
+    @Override
+    public String run(int times) {
+        for (int i = 0; i < times; i++){
+
+        }
+
+        return null;
+    }
+}
